7387963705c945ed9b69b31e78473c789aaef7b9
Evan Edgell <edgele1@mcmaster.ca>
2025-05-09 11:27:29 -0400

    1.1-1.5 R for Data Science practice

--- test r script.R
+++ test r script.R
@@ -1,1 +1,8 @@
-example("plot")
+library(palmerpenguins)
+
+ggplot(penguins, aes(x = body_mass_g)) +
+  geom_histogram(bins = 10)
+
+ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g)) +
+  geom_point(aes(color = species, shape = species)) +
+  facet_wrap(~island)
